94136dc18ca24d869cef8239bc0a1e430047ea5f
jenya <jeynet2020@gmail.com>
2019-04-15 17:18:13 +0300

    starting project

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      minCss = require('gulp-clean-css'),
+      rename = require('gulp-rename'),
+      plumber = require("gulp-plumber");
+
+
+// style
+gulp.task('style', function(){
+  return gulp
+    .src('app/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest("app/css/"))
+    .pipe(rename('style.min.css'))
+    .pipe(minCss())
+    .pipe(gulp.dest("app/css/"))
+});
+
+
+//  watch
+gulp.task("watch", function() {
+  gulp.watch("app/scss/*.scss", gulp.parallel("style"))
+})
+
+
+// defoult
+gulp.task("default", gulp.parallel("style", "watch"));
