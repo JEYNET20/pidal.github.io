4b2cd67e4c0e9de12b4c44c6769fc86607cda135
jenya <jeynet2020@gmail.com>
2019-06-30 22:37:14 +0300

    работаю над стр сообщения

--- app/js/my-scroll.js
+++ app/js/my-scroll.js
@@ -2,5 +2,5 @@
 (function(){
   // скрол для кабинета блока с сообщением
   $(".cabinet .message-content__block .body").niceScroll();
-
+	$(".cabinet .message-container-wrapper .body").niceScroll();
 })();
